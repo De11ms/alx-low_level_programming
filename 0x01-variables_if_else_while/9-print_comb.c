ffb50220aeb24e61195e6f715688d2a282f27500
Biniam <biniammesfin54@gmail.com>
2022-12-09 11:10:06 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <time.h>
+
+/**
+ * main - entry
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+        {
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
