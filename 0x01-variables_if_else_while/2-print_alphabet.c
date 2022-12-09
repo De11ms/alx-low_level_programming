72c25037e761a5226826563ddec98f06f7146496
Biniam <biniammesfin54@gmail.com>
2022-12-09 07:21:27 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - main function to generate random a number
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int ch = 'a';
+	while (ch <= '2')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
