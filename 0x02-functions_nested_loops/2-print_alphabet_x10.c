b3336de28847f7a106649b72ab9d665bc637a5d6
Biniam <biniammesfin54@gmail.com>
2022-12-13 08:07:14 -0800

    task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int j;
+	int x;
+
+	for (x = o; x < 10; x++)
+	{
+        	for (j = 'a'; j <= 'z'; j++)
+        	{
+	        	_putchar(j)
+        	}
+        	putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - entry
+ * Return: returns 1 and 0 depending on condition
+ */
+int _islower(int c);
+
+{
+
+	return (c >= 'a' && c <= 'z')
+
+}
