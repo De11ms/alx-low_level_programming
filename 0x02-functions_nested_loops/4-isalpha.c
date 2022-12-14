1e13942dfc15804665149fcf9e2d18c372b90d81
Biniam <biniammesfin54@gmail.com>
2022-12-14 07:52:14 -0800

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,10 +5,10 @@
  * _isalpha - checks for alphabetical letters
  * @: a character to be check on
  * Return: returns 0 or 1 depending on condition
+ * c is alphabet
  */
 int _isalpha(int c)
 {
 
 	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
-	  
 }
