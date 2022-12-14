7aba6039f9685060ed05fcf3654e4853cbae8466
Biniam <biniammesfin54@gmail.com>
2022-12-14 11:46:46 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,20 +6,22 @@
  * @n : number to start from
  * Return:0 or 1
  */
-
+i
 void print_to_98(int n)
 {
-	while (n < 98);
-	{
-		printf("%i, ", n);
-        	n++;
-	}
-	while (n > 98)
-	{
-		printf("%i, " , n);
-		n--;
-	}
-	printf("98");
-	putchar('\n');
+
+while (n < 98);
+{
+printf("%i, ", n);
+n++;
+}
+while (n > 98)
+{
+	printf("%i, ", n);
+	n--;
+}
+
+printf("98");
+putchar('\n');
 }
 
