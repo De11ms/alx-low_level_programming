b79d93f4c76d3563f7929f765d3698eef6512cb2
Biniam <biniammesfin54@gmail.com>
2022-12-15 07:58:55 -0800

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if numbers are 0 - 9
+ * @c: character to check
+ */
+
+int _isdigit(int c)
+{
+if (c >= '0' && <= '9')
+return (1);
+else
+return (0);
+}
