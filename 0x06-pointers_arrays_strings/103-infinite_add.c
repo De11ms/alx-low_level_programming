7852251d16fabff923e4022beb87112fdccdefa2
Biniam <biniammesfin54@gmail.com>
2022-12-22 09:33:13 -0800

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -17,7 +17,7 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
 	int num, tens = 0;
 
-	for (; *n1, && *n2; n1--, n2--, r_index--)
+	for (; *n1 && *n2; n1--, n2--, r_index--)
 	{
 		num = (*n1 - '0') + (*n2 - '0');
 		num += tens;
@@ -34,17 +34,19 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 	for (; *n2; n2--, r_index--)
 	{
 		num = (*n2 - '0') + tens;
-		*r(r + r_index) = (num % 10) + '0';
+		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
 
 	if (tens && r_index >= 0)
 	{
 		*(r + r_index) = (tens % 10) + '0';
-		return (r + r index);
+		return (r + r_index);
 	}
+
 	else if (tens && r_index < 0)
 		return (0);
+
 	return (r + r_index + 1);
 }
 /**
@@ -69,6 +71,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
 		return (0);
+
         n1 += n1_len - 1;
 	n2 += n2_len - 1;
 	*(r + size_r) = '\0';
