9d6e153371a5e90683130e8fa9a1b73c2875869c
Biniam <biniammesfin54@gmail.com>
2022-12-22 07:22:27 -0800

    task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.>
+#include <stdio.h>
 
 /**
  * print_buffer - prints a buffer 10 at a time
@@ -11,6 +11,8 @@ void print_buffer(char *b, int size)
 	int byte, index;
 
 	for (byte = 0; byte < size; byte += 10)
+	{
+		printf("%08x: ", byte);
 
 		for (index = 0; index < 10; index++)
 		{
@@ -22,24 +24,23 @@ void print_buffer(char *b, int size)
 			if ((index % 2) != 0 && index != 0)
 				printf(" ");
 		}
-	for (index = 0; index < 10; index++)
-	{
-		if ((index + byte) >= size)
-			break;
+	        for (index = 0; index < 10; index++)
+	        {
+			if ((index + byte) >= size)
+				break;
 
-		else if (*(b + index + byte) >= 31 &&
-		 	 *(b + index + byte) <=126)
-			printf("%c", *(b + index + byte));
-		else
-			printf(" ");
-	}
+		        else if (*(b + index + byte) >= 31 &&
+		 	         *(b + index + byte) <=126)
+			        printf("%c", *(b + index + byte));
+		        else
+				printf(" ");
+		}
 
-	if (byte >= size)
-		continue;
-	printf("\n");
+	        if (byte >= size)
+			continue;
+	        printf("\n");
         }
-
-         if (size <= 0)
-	printf("\n");
+        if (size <= 0)
+		printf("\n");
 }
 
