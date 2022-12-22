2fb73d573168efe116778cdfd8c9a06fbe9d7df4
Biniam <biniammesfin54@gmail.com>
2022-12-22 08:36:02 -0800

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -24,14 +24,14 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
-	for (; *n1--, r_index--)
+	for (; *n1; n1--, r_index--)
 	{
 		num = (*n1 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 
 	}
-	for (;  *n2; n2--, r_index--)
+	for (; *n2; n2--, r_index--)
 	{
 		num = (*n2 - '0') + tens;
 		*r(r + r_index) = (num % 10) + '0';
@@ -70,7 +70,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
 		return (0);
         n1 += n1_len - 1;
-	n2 += n1_len - 1;
+	n2 += n2_len - 1;
 	*(r + size_r) = '\0';
 
 	return (add_strings(n1, n2, r, --size_r));
