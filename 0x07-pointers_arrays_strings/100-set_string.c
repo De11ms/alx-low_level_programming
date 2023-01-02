dba4f68119a96e35b791cbcfed882b286477e2b5
Biniam <biniammesfin54@gmail.com>
2023-01-02 03:54:14 -0800

    task 8

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * srt_string - sets the value of pointer
+ * set_string - sets the value of pointer
  * @s: the pointer
  * @to: char
+ * Return: no return
  */
 void set_string(char **s, char *to)
 {
