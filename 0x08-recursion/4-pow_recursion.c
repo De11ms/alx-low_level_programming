a4d233646500074e748d1d7469bf5130d3086904
Biniam <biniammesfin54@gmail.com>
2023-01-04 05:46:38 -0800

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,6 @@
  * _pow_recursion - returns the value of x to the power of y
  * @x: number
  * @y: power
- *  
  * Return: x to the power of y
  * -1 if x < 0
  */
@@ -16,5 +15,5 @@ int _pow_recursion(int x, int y)
 		return (1);
 	if (y == 1)
 		return (x);
-        return (x * _pow_recursion(x, y - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
