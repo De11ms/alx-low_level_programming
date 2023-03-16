6fadc155cae3ee653500df270e57cd129d1c080c
Biniam <biniammesfin54@gmail.com>
2023-03-16 09:11:56 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - program that prints line
+ * Return 1 (success)
+ */
+int main(void)
+{
+        fput("and that piece of art is useful\" - Dora Kor
+par,2015-10-19\n", stdout);
+return (1)
+}
