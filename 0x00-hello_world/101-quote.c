d8dba3c7d15abe43a19ffd4130f20093e12fe7b1
Biniam <biniammesfin54@gmail.com>
2023-03-16 09:48:53 -0700

    task1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 /**
  * main - program that prints line
+ *
  * Return 1 (success)
  */
 int main(void)
 {
         write(2,
-	"and that piece of art is useful\" - Dora Kor
-par,2015-10-19\n", 59);
-return (1);
+	"and that piece of art is useful\" - Dora Korpar,2015-10-
+19\n", 59);
+        return (1);
 }
