fc587f0dc89978cf75d3b052d8e54a24160e4d51
Biniam <biniammesfin54@gmail.com>
2023-03-17 11:52:29 -0700

    tas

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,13 +4,12 @@
 
 /**
  * main - returns alphabets both except q and e
- *
- * Return: Always 0 (Success) 
- *
+ * Return: Always 0 (success) 
  */
 int main(void)
 {
 	char ch = 'a';
+
 	while (ch <= 'z')
 	{
 		if ((ch != 'e') && (ch != 'q'))
