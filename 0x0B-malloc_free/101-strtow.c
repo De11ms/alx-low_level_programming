c7b82cc0ac224440d8e2bddc4a60b0c1eec82ac4
Biniam <biniammesfin54@gmail.com>
2023-04-10 02:47:11 -0700

    ta

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,81 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * count_word - helper function
+ * @s: string to evaluate
+ *
+ * Return: number of words
+ */
+int count_word(char *s)
+{
+	int flag, c, w;
+
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\C'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+
+	return (0);
+}
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (success)
+ * or NULL (Error)
+ */
+char **strtow(char *str)
+{
+	char **matrix, *temp;
+
+	int i, k = 0, len = 0,words, c = 0, start, end;
+
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix = NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (str[1] == ' ' || str[i] == '\0')
+
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*temp = '\0';
+				matrix[k] = temp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+
+	matrix[k] = NULL;
+
+	return (matrix);
+}
+
